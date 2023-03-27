da410974e2e9ecbf462dd488171840a61747daee
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 02:17:46 +0330

    wrap redux on app

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,18 @@ import { Route, Routes } from "react-router-dom";
 // components
 import Landing from "./pages/landing/landing";
 import Products from "./pages/products/products";
+// redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const Home = () => {
   return (
-    <>
+    <Provider store={store}>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/products" element={<Products />} />
       </Routes>
-    </>
+    </Provider>
   );
 };
 
