44c9cf40095e3ce9499d4b6c3f32361d6601fc94
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 02:39:30 +0330

    fetching products and mapping on them

--- src/components/modules/home/products.jsx
+++ src/components/modules/home/products.jsx
@@ -1,8 +1,41 @@
 // react
-import React from "react";
+import React, { useEffect } from "react";
+// redux
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProducts } from "../../../redux/products/productsAction";
+// chakra ui
+import { Flex } from "@chakra-ui/react";
+// components
+import ProductCard from "./cards/productCard";
 
 const HomePageProducts = () => {
-  return <h1>products homepage</h1>;
+  const dispatch = useDispatch();
+  const productsState = useSelector((state) => state.productsState);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      {productsState.loading ? (
+        <h2>Loading...</h2>
+      ) : productsState.error ? (
+        <p>something went wrong.</p>
+      ) : (
+        <Flex
+          justifyContent={{ base: "center", md: "space-between" }}
+          flexWrap="wrap"
+          gap={3}
+          my={10}
+        >
+          {productsState.products.map((product) => (
+            <ProductCard />
+          ))}
+        </Flex>
+      )}
+    </>
+  );
 };
 
 export default HomePageProducts;
