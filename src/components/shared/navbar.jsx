f909e822b51e9dcfa048bb547e0a414500a7b946
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 00:22:05 +0330

    create navbar components

--- src/components/shared/navbar.jsx
+++ src/components/shared/navbar.jsx
@@ -1,8 +1,19 @@
 // react
 import React from "react";
+// chakra ui
+import { Flex, Heading } from "@chakra-ui/react";
+//icons
+import { RiShoppingCart2Line } from "react-icons/ri";
 
 const Navbar = () => {
-  return <h1>navbar</h1>;
+  return (
+    <Flex justifyContent="space-between" alignItems="center" h="14" p={10}>
+      <Heading as="h2" color="primaryColor">
+        droplink
+      </Heading>
+      <RiShoppingCart2Line color="#fff" size={35} />
+    </Flex>
+  );
 };
 
 export default Navbar;
