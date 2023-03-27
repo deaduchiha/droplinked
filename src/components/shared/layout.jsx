4e9b30a00b587d5a81192b572d4881aa58660bf1
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 00:08:10 +0330

    create layout components

---
+++ src/components/shared/layout.jsx
@@ -0,0 +1,8 @@
+import { Container } from "@chakra-ui/react";
+import React from "react";
+
+const Layout = ({ children }) => {
+  return <Container maxW="container.lg">layout</Container>;
+};
+
+export default Layout;
