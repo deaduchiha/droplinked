0fc016b13af7c2a0261cf05ee80a3c9885085905
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 00:14:12 +0330

    wrap layout into the project

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./styles/theme";
 // react router dom
 import { BrowserRouter as Router } from "react-router-dom";
+// layout
+import Layout from "./components/shared/layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider theme={theme}>
     <Router>
-      <Home />
+      <Layout>
+        <Home />
+      </Layout>
     </Router>
   </ChakraProvider>
 );
