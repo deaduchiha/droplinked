8c649a881ac5ee95ed63ede666ecf3a0de012c20
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-27 23:53:36 +0330

    change app path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // components
-import App from "./App";
+import App from "./pages/App";
 // chakra ui
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./styles/theme";
