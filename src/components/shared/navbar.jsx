a64795580b3bfe6274f517d4a99d7fe813bf0787
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 00:32:37 +0330

    complete navbar component

--- src/components/shared/navbar.jsx
+++ src/components/shared/navbar.jsx
@@ -1,18 +1,45 @@
 // react
-import React from "react";
+import React, { useState } from "react";
 // chakra ui
-import { Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 //icons
 import { RiShoppingCart2Line } from "react-icons/ri";
 
 const Navbar = () => {
+  const [qunatity, setQuantity] = useState(false);
   return (
-    <Flex justifyContent="space-between" alignItems="center" h="14" p={10}>
-      <Heading as="h2" color="primaryColor">
-        droplink
-      </Heading>
-      <RiShoppingCart2Line color="#fff" size={35} />
-    </Flex>
+    <>
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+        h="14"
+        p={10}
+        borderBottom="1px"
+        borderColor="secondaryColor"
+        position="relative"
+      >
+        <Heading as="h2" color="primaryColor">
+          droplink
+        </Heading>
+        <RiShoppingCart2Line color="#fff" size={35}></RiShoppingCart2Line>
+        <Flex
+          borderRadius="lg"
+          borderBottomRightRadius={0}
+          bg="primaryColor"
+          pos="absolute"
+          top={6}
+          right={9}
+          w="15px"
+          h="15px"
+          justifyContent="center"
+          alignItems="center"
+          fontSize="11"
+          userSelect="none"
+        >
+          2
+        </Flex>
+      </Flex>
+    </>
   );
 };
 
