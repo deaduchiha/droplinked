739f66afc7090aaf85b88f15b23de0b3a884e950
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 12:12:52 +0330

    create slider

--- src/components/modules/details/detailsImage.jsx
+++ src/components/modules/details/detailsImage.jsx
@@ -1,16 +1,51 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
-import React from "react";
+// react
+import React, { useState } from "react";
+// chakra ui
+import { Box, Image } from "@chakra-ui/react";
+// swiper
+import { Pagination, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const DetailsImage = ({ data }) => {
   const { images } = data;
+  const [showImage, setShowImage] = useState([images[0].src]);
+
   return (
-    <Box overflow="hidden">
-      <Flex gap={20}>
+    <Box>
+      <Swiper className="mySwiper">
+        <SwiperSlide>
+          <Image
+            w="349px"
+            src={showImage}
+            alt="image"
+            mb={10}
+            userSelect="none"
+          />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        effect={"flip"}
+        pagination={true}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+        slidesPerView={3}
+      >
         {images.map((img) => (
-          <Image w="72" src={img.src} alt="image" />
+          <SwiperSlide key={img.id}>
+            <Image
+              userSelect="none"
+              w="20"
+              src={img.src}
+              alt="image"
+              onClick={() => setShowImage(img.src)}
+            />
+          </SwiperSlide>
         ))}
-      </Flex>
-      <Box></Box>
+      </Swiper>
     </Box>
   );
 };
