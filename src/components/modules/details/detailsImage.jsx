132cab28029b209b6867eb942c61f1b372a8ad76
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 11:30:14 +0330

    install swiper

--- src/components/modules/details/detailsImage.jsx
+++ src/components/modules/details/detailsImage.jsx
@@ -1,54 +1,16 @@
-import { Box, Flex, Image, Text, UnorderedList } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
-import Slider from "react-slick";
 
 const DetailsImage = ({ data }) => {
   const { images } = data;
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    appendDots: () => (
-      <Flex
-        w="full"
-        gap={5}
-        justifyContent="space-between"
-        alignItems={"center"}
-      >
-        <Text>Next</Text>
-        {images.map((img) => (
-          <Image
-            src={img.src}
-            alt={data.title}
-            key={img.id}
-            w={"4"}
-            borderRadius={5}
-          />
-        ))}
-        <Text>back</Text>
-      </Flex>
-    ),
-    customPaging: (i) => (
-      <Box width="30px" color="blue" border="1px blue solid" textAlign="center">
-        {i + 1}
-      </Box>
-    ),
-  };
-
   return (
     <Box overflow="hidden">
-      <h2>Append Dots</h2>
-      <Slider {...settings}>
+      <Flex gap={20}>
         {images.map((img) => (
-          <Flex>
-            <Image src={img.src} alt="image" />
-          </Flex>
+          <Image w="72" src={img.src} alt="image" />
         ))}
-        <Box></Box>
-      </Slider>
+      </Flex>
+      <Box></Box>
     </Box>
   );
 };
