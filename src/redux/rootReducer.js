358951d28173bee5dc88e8ca1db82ec0714373fa
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 12:41:41 +0330

    create cart redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import productsReducer from "./products/productsReducer";
+import cartReducer from "./cart/CartReducer";
 
 const rootReducer = combineReducers({
   productsState: productsReducer,
+  cartsState: cartReducer,
 });
 
 export default rootReducer;
