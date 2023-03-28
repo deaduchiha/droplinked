b82e15cc8b9019b69db0f2082b93f3f3a638e6dc
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 10:33:16 +0330

    fetch product details

--- src/components/modules/details/productDetails.jsx
+++ src/components/modules/details/productDetails.jsx
@@ -1,27 +1,35 @@
 // react
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // chakra ui
 import { Flex } from "@chakra-ui/react";
 //react router dom
 import { useParams } from "react-router-dom";
-// axios
+// redux
 import axios from "axios";
 
 const ProductDetail = () => {
+  const [data, setData] = useState({});
   const id = useParams().id;
 
   useEffect(() => {
-    axios
-      .get(`https://apiv2.droplinked.com/product/public/${id}`)
-      .then((response) => {
-        const data = response.data.data.shopifyData;
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [id]);
+    if (!data.length) {
+      axios
+        .get(`https://apiv2.droplinked.com/product/public/${id}`)
+        .then((response) => {
+          const data = response.data.data.shopifyData;
+          console.log(data);
+          setData(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [data.length, id]);
 
-  return <Flex h="38em" mt={5} bg="secondaryColor"></Flex>;
+  return (
+    <Flex h="38em" mt={5} bg="secondaryColor">
+      {data.title}
+    </Flex>
+  );
 };
 export default ProductDetail;
