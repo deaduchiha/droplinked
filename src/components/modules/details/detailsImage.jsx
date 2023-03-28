3cfdf352ee7673aea5395627c2fae44ca9d5191f
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 11:25:42 +0330

    uninstall slick

--- src/components/modules/details/detailsImage.jsx
+++ src/components/modules/details/detailsImage.jsx
@@ -1,7 +1,56 @@
+import { Box, Flex, Image, Text, UnorderedList } from "@chakra-ui/react";
 import React from "react";
+import Slider from "react-slick";
 
 const DetailsImage = ({ data }) => {
-  return <h1>{data.title} </h1>;
+  const { images } = data;
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendDots: () => (
+      <Flex
+        w="full"
+        gap={5}
+        justifyContent="space-between"
+        alignItems={"center"}
+      >
+        <Text>Next</Text>
+        {images.map((img) => (
+          <Image
+            src={img.src}
+            alt={data.title}
+            key={img.id}
+            w={"4"}
+            borderRadius={5}
+          />
+        ))}
+        <Text>back</Text>
+      </Flex>
+    ),
+    customPaging: (i) => (
+      <Box width="30px" color="blue" border="1px blue solid" textAlign="center">
+        {i + 1}
+      </Box>
+    ),
+  };
+
+  return (
+    <Box overflow="hidden">
+      <h2>Append Dots</h2>
+      <Slider {...settings}>
+        {images.map((img) => (
+          <Flex>
+            <Image src={img.src} alt="image" />
+          </Flex>
+        ))}
+        <Box></Box>
+      </Slider>
+    </Box>
+  );
 };
 
 export default DetailsImage;
