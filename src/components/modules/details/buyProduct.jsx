7b2bace2025104d6550c6099b7be383695070f9f
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-28 13:08:58 +0330

    complete buy product

--- src/components/modules/details/buyProduct.jsx
+++ src/components/modules/details/buyProduct.jsx
@@ -1,10 +1,28 @@
 // react
-import { Heading } from "@chakra-ui/react";
+import { Button, Table, TableContainer, Tbody, Td, Tr } from "@chakra-ui/react";
 import React from "react";
 
 const BuyProduct = ({ data }) => {
-  console.log(data);
-  return <Heading>Buy product2</Heading>;
+  const { variants } = data;
+
+  return (
+    <TableContainer>
+      <Table borderColor="red !important">
+        <Tbody>
+          {variants.map((variant) => (
+            <Tr key={variant.id}>
+              <Td>Price: {`${variant.formatted_price}`}</Td>
+              <Td>Quantity: {`${Math.abs(variant.inventory_quantity)}`}</Td>
+              <Td>Weight: {`${variant.weight}`}</Td>
+              <Td isNumeric>
+                <Button colorScheme="green">Request</Button>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default BuyProduct;
